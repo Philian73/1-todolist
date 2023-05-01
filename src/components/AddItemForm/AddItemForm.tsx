c428d716af7429257fe55cb60aca79097c6d6133
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-01 21:49:59 +0400

    Added logic

--- src/components/AddItemForm/AddItemForm.tsx
+++ src/components/AddItemForm/AddItemForm.tsx
@@ -1,6 +1,46 @@
-import { FC } from 'react'
+import { ChangeEvent, KeyboardEvent, FC, useState } from 'react'
 
-type PropsType = {}
-export const AddItemForm: FC<PropsType> = ({}) => {
-  return <div></div>
+import { SuperButton } from '../SuperButton/SuperButton'
+import s from '../TodoList/TodoList.module.css'
+
+type PropsType = {
+  addItem: (title: string) => void
+}
+export const AddItemForm: FC<PropsType> = ({ addItem }) => {
+  const [title, setTitle] = useState('')
+  const [error, setError] = useState<string | null>(null)
+
+  const addItemCallback = () => {
+    if (error) return
+
+    if (title.trim()) {
+      addItem(title)
+      setTitle('')
+    } else {
+      setError('Title is required')
+    }
+  }
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => setTitle(e.currentTarget.value)
+  const onKeyDownHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && e.ctrlKey) addItemCallback()
+    if (error && e.key !== ' ') setError(null)
+  }
+
+  return (
+    <div>
+      <input
+        className={error ? s.error : ''}
+        value={title}
+        onChange={onChangeHandler}
+        onKeyDown={onKeyDownHandler}
+      />
+      <SuperButton name="+" onClick={addItemCallback} />
+      {error && (
+        <div>
+          <span className={s.errorMessage}>{error}</span>
+        </div>
+      )}
+    </div>
+  )
 }
