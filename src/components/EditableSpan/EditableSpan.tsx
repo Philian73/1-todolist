ea1f0793ddca2e63c03b77eece1812ae7e784f85
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-02 23:33:58 +0400

    Made a universal component

--- src/components/EditableSpan/EditableSpan.tsx
+++ src/components/EditableSpan/EditableSpan.tsx
@@ -1,6 +1,27 @@
-import { FC } from 'react'
+import { ChangeEvent, FC, useState } from 'react'
 
-type PropsType = {}
-export const EditableSpan: FC<PropsType> = ({}) => {
-  return <div></div>
+type PropsType = {
+  title: string
+  changeTitle: (title: string) => void
+}
+export const EditableSpan: FC<PropsType> = ({ title, changeTitle }) => {
+  const [inputValue, setInputValue] = useState('')
+  const [editMode, setEditMode] = useState(false)
+
+  const activateEditMode = () => {
+    setInputValue(title)
+    setEditMode(true)
+  }
+  const activateViewMode = () => {
+    changeTitle(inputValue)
+    setEditMode(false)
+  }
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => setInputValue(e.currentTarget.value)
+
+  return editMode ? (
+    <input type="text" value={inputValue} onChange={onChangeHandler} onBlur={activateViewMode} />
+  ) : (
+    <span onDoubleClick={activateEditMode}>{title}</span>
+  )
 }
