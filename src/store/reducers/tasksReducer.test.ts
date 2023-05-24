08adf4fc3a925fcdfdb091eae10a63efff92659d
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-24 23:53:01 +0400

    Added tests for 'removeTodoList' and 'addTodoList'

--- src/store/reducers/tasksReducer.test.ts
+++ src/store/reducers/tasksReducer.test.ts
@@ -1,6 +1,7 @@
 import { TasksType } from '../../App.tsx'
 
 import { tasksActions, tasksReducer } from './tasksReducer.ts'
+import { todoListsActions } from './todoListsReducer.ts'
 
 test('correct task should be deleted from correct array', () => {
   const initialState: TasksType = {
@@ -34,7 +35,7 @@ test('correct task should be deleted from correct array', () => {
 })
 
 test('correct task should be added to correct array', () => {
-  const startState: TasksType = {
+  const initialState: TasksType = {
     todoListID_1: [
       { id: '1', title: 'CSS', isDone: false },
       { id: '2', title: 'JS', isDone: true },
@@ -50,7 +51,7 @@ test('correct task should be added to correct array', () => {
 
   const action = tasksActions.addTask('todoListID_2', newTaskTitle)
 
-  const endState = tasksReducer(startState, action)
+  const endState = tasksReducer(initialState, action)
 
   expect(endState['todoListID_1']).toHaveLength(3)
   expect(endState['todoListID_2']).toHaveLength(4)
@@ -60,7 +61,7 @@ test('correct task should be added to correct array', () => {
 })
 
 test('title of specified task should be changed', () => {
-  const startState: TasksType = {
+  const initialState: TasksType = {
     todoListID_1: [
       { id: '1', title: 'CSS', isDone: false },
       { id: '2', title: 'JS', isDone: true },
@@ -76,14 +77,14 @@ test('title of specified task should be changed', () => {
 
   const action = tasksActions.changeTitleTask('todoListID_2', '2', newTitleTask)
 
-  const endState = tasksReducer(startState, action)
+  const endState = tasksReducer(initialState, action)
 
   expect(endState['todoListID_1'][1].title).toBe('JS')
   expect(endState['todoListID_2'][1].title).toBe(newTitleTask)
 })
 
 test('status of specified task should be changed', () => {
-  const startState: TasksType = {
+  const initialState: TasksType = {
     todoListID_1: [
       { id: '1', title: 'CSS', isDone: false },
       { id: '2', title: 'JS', isDone: true },
@@ -98,8 +99,61 @@ test('status of specified task should be changed', () => {
 
   const action = tasksActions.changeStatusTask('todoListID_2', '2', false)
 
-  const endState = tasksReducer(startState, action)
+  const endState = tasksReducer(initialState, action)
 
   expect(endState['todoListID_1'][1].isDone).toBeTruthy()
   expect(endState['todoListID_2'][1].isDone).toBeFalsy()
+})
+
+test('property with todolistId should be deleted', () => {
+  const initialState: TasksType = {
+    todoListID_1: [
+      { id: '1', title: 'CSS', isDone: false },
+      { id: '2', title: 'JS', isDone: true },
+      { id: '3', title: 'React', isDone: false },
+    ],
+    todoListID_2: [
+      { id: '1', title: 'bread', isDone: false },
+      { id: '2', title: 'milk', isDone: true },
+      { id: '3', title: 'tea', isDone: false },
+    ],
+  }
+
+  const action = todoListsActions.removeTodoList('todoListID_2')
+
+  const endState = tasksReducer(initialState, action)
+
+  const keys = Object.keys(endState)
+
+  expect(keys).toHaveLength(1)
+  expect(endState['todoListID_2']).toBeUndefined()
+})
+
+test('new array should be added when new todolist is added', () => {
+  const initialState: TasksType = {
+    todoListID_1: [
+      { id: '1', title: 'CSS', isDone: false },
+      { id: '2', title: 'JS', isDone: true },
+      { id: '3', title: 'React', isDone: false },
+    ],
+    todoListID_2: [
+      { id: '1', title: 'bread', isDone: false },
+      { id: '2', title: 'milk', isDone: true },
+      { id: '3', title: 'tea', isDone: false },
+    ],
+  }
+
+  const action = todoListsActions.addTodoList('New TodoList')
+
+  const endState = tasksReducer(initialState, action)
+
+  const keys = Object.keys(endState)
+  const newKey = keys.find(key => key !== 'todoListID_1' && key !== 'todoListID_2')
+
+  if (!newKey) {
+    throw Error('new key should be added')
+  }
+
+  expect(keys).toHaveLength(3)
+  expect(endState[newKey]).toEqual([])
 })
