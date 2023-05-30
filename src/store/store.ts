4b872bf7625f6c8c3b65b6cc28dedf58de660afb
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-30 22:19:12 +0400

    Create store (legacy_createStore), rootReducer and type 'AppRootStateType'

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,3 +1,17 @@
+import { combineReducers, legacy_createStore } from 'redux'
+
+import { tasksReducer } from './reducers/tasksReducer.ts'
+import { todoListsReducer } from './reducers/todoListsReducer.ts'
+
 export type InferActionTypes<T> = T extends { [keys: string]: (...args: any[]) => infer U }
   ? U
   : never
+
+export type AppRootStateType = ReturnType<typeof rootReducer>
+
+const rootReducer = combineReducers({
+  tasks: tasksReducer,
+  todoLists: todoListsReducer,
+})
+
+export const store = legacy_createStore(rootReducer)
