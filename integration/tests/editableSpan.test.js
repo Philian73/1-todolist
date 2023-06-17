60f84ccc61699b0facf5c94068afddce8c562ab0
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-06-17 17:20:26 +0400

    Added snapshot test from EditableSpan (EditableSpan => Base story)

---
+++ integration/tests/editableSpan.test.js
@@ -0,0 +1,14 @@
+describe('EditableSpan snapshot test', () => {
+  it('base example, visually looks correct', async () => {
+    // APIs from jest-puppeteer
+    await page.goto(
+      'http://localhost:6006/iframe.html?args=&id=todolists-editablespan--base&viewMode=story',
+      { waitUntil: 'networkidle2' }
+    )
+
+    const image = await page.screenshot()
+
+    // API from jest-image-snapshot
+    expect(image).toMatchImageSnapshot()
+  })
+})
