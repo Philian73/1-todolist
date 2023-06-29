13a041572917f11af176f7fea915e1424ce4f92a
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-06-29 23:28:35 +0300

    applyMiddleware(thunk)

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,5 @@
-import { AnyAction, combineReducers, compose, legacy_createStore } from 'redux'
-import { ThunkDispatch } from 'redux-thunk'
+import { AnyAction, applyMiddleware, combineReducers, compose, legacy_createStore } from 'redux'
+import thunk, { ThunkDispatch } from 'redux-thunk'
 
 import { tasksReducer } from './reducers/tasksReducer.ts'
 import { todoListsReducer } from './reducers/todoListsReducer.ts'
@@ -21,4 +21,4 @@ const rootReducer = combineReducers({
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-export const store = legacy_createStore(rootReducer, composeEnhancers())
+export const store = legacy_createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
