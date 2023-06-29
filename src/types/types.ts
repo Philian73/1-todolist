8b300e494f608315d1d02062bfb4e35f9318d7f4
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-06-29 23:59:56 +0300

    refactor TodoListType and add TodoListDomainType

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,8 +3,11 @@ export type FilterValuesType = 'all' | 'active' | 'completed'
 export type TodoListType = {
   id: string
   title: string
-  filter: FilterValuesType
+  addedDate: string
+  order: number
 }
+
+export type TodoListDomainType = TodoListType & { filter: FilterValuesType }
 
 export type TaskType = {
   id: string
