0ea82911bcb979b46c89e45a6d8502eafb3e3b8f
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-02 00:55:50 +0300

    Import 'updateTask' thunk creator and refactor

--- src/components/TodoList/Task/Task.tsx
+++ src/components/TodoList/Task/Task.tsx
@@ -6,7 +6,7 @@ import IconButton from '@mui/material/IconButton'
 import ListItem from '@mui/material/ListItem'
 
 import { useAppDispatch } from '../../../hooks/hooks.ts'
-import { deleteTask, tasksActions } from '../../../store/reducers/tasksReducer.ts'
+import { deleteTask, updateTask } from '../../../store/reducers/tasksReducer.ts'
 import { TaskStatuses, TaskType } from '../../../types/types.ts'
 import { EditableSpan } from '../../EditableSpan/EditableSpan.tsx'
 
@@ -23,16 +23,20 @@ export const Task: FC<PropsType> = memo(({ todoListID, task }) => {
 
   const changeStatusTask = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
-      dispatch(tasksActions.changeStatusTask(todoListID, task.id, e.currentTarget.checked))
+      dispatch(
+        updateTask(todoListID, task.id, {
+          status: e.currentTarget.checked ? TaskStatuses.Completed : TaskStatuses.New,
+        })
+      )
     },
-    [dispatch, task.id]
+    [dispatch, todoListID, task.id]
   )
 
   const changeTitleTask = useCallback(
     (newTitle: string) => {
-      dispatch(tasksActions.changeTitleTask(todoListID, task.id, newTitle))
+      dispatch(updateTask(todoListID, task.id, { title: newTitle }))
     },
-    [dispatch, task.id]
+    [dispatch, todoListID, task.id]
   )
 
   return (
