9897d87e8db8730248024c1c15ec25f8b663bfcc
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-01 23:35:51 +0300

    Added getTasks method and GetTasksResponseType

--- src/api/todoListsAPI.ts
+++ src/api/todoListsAPI.ts
@@ -1,9 +1,18 @@
-import { TodoListType } from '../types/types.ts'
+import { TaskType, TodoListType } from '../types/types.ts'
 
 import { instance } from './api.ts'
+
+type GetTasksResponseType = {
+  error: string | null
+  totalCount: number
+  items: TaskType[]
+}
 
 export const todoListsAPI = {
   getTodoLists() {
     return instance.get<TodoListType[]>('todo-lists')
+  },
+  getTasks(todoListID: string) {
+    return instance.get<GetTasksResponseType>(`todo-lists/${todoListID}/tasks`)
   },
 }
