e7e81bfa88ea0065adbc8687c3abd4d31cc62395
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-01 23:45:49 +0300

    Add method: 'deleteTask'

--- src/api/todoListsAPI.ts
+++ src/api/todoListsAPI.ts
@@ -1,6 +1,6 @@
 import { TaskType, TodoListType } from '../types/types.ts'
 
-import { instance } from './api.ts'
+import { instance, ResponseType } from './api.ts'
 
 type GetTasksResponseType = {
   error: string | null
@@ -14,5 +14,8 @@ export const todoListsAPI = {
   },
   getTasks(todoListID: string) {
     return instance.get<GetTasksResponseType>(`todo-lists/${todoListID}/tasks`)
+  },
+  deleteTask(todoListID: string, taskID: string) {
+    return instance.delete<ResponseType>(`todo-lists/${todoListID}/tasks/${taskID}`)
   },
 }
