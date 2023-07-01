f75f3d23a0d691c8d65a76bd15625b3db2ef0ec6
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-01 23:59:37 +0300

    Refactor TaskType and added: TaskStatuses & TaskPriorities enum

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,18 @@
+export enum TaskStatuses {
+  New = 0,
+  InProgress = 1,
+  Completed = 2,
+  Draft = 3,
+}
+
+export enum TaskPriorities {
+  Low = 0,
+  Middle = 1,
+  Hi = 2,
+  Urgently = 3,
+  Later = 4,
+}
+
 export type FilterValuesType = 'all' | 'active' | 'completed'
 
 export type TodoListType = {
@@ -10,9 +25,16 @@ export type TodoListType = {
 export type TodoListDomainType = TodoListType & { filter: FilterValuesType }
 
 export type TaskType = {
-  id: string
+  description: string
   title: string
-  isDone: boolean
+  status: TaskStatuses
+  priority: TaskPriorities
+  startDate: string
+  deadline: string
+  id: string
+  todoListId: string
+  order: number
+  addedDate: string
 }
 
 export type TasksType = {
