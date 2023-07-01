60188240727057d616140c9e7a60eb617c2dc5ab
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-02 00:31:56 +0300

    refactor

--- src/store/reducers/tasksReducer.test.ts
+++ src/store/reducers/tasksReducer.test.ts
@@ -14,14 +14,80 @@ describe('tasksReducer', () => {
 
     initialState = {
       [todoListID_1]: [
-        { id: '1', title: 'CSS', isDone: false },
-        { id: '2', title: 'JS', isDone: true },
-        { id: '3', title: 'React', isDone: false },
+        {
+          id: '1',
+          title: 'CSS',
+          status: 0,
+          addedDate: new Date().toISOString(),
+          deadline: null,
+          description: null,
+          startDate: new Date().toISOString(),
+          priority: 1,
+          order: -2,
+          todoListId: todoListID_1,
+        },
+        {
+          id: '2',
+          title: 'JS',
+          status: 2,
+          addedDate: new Date().toISOString(),
+          deadline: null,
+          description: null,
+          startDate: new Date().toISOString(),
+          priority: 1,
+          order: -1,
+          todoListId: todoListID_1,
+        },
+        {
+          id: '3',
+          title: 'React',
+          status: 0,
+          addedDate: new Date().toISOString(),
+          deadline: null,
+          description: null,
+          startDate: new Date().toISOString(),
+          priority: 1,
+          order: 0,
+          todoListId: todoListID_1,
+        },
       ],
       [todoListID_2]: [
-        { id: '1', title: 'bread', isDone: false },
-        { id: '2', title: 'milk', isDone: true },
-        { id: '3', title: 'tea', isDone: false },
+        {
+          id: '1',
+          title: 'bread',
+          status: 0,
+          addedDate: new Date().toISOString(),
+          deadline: null,
+          description: null,
+          startDate: new Date().toISOString(),
+          priority: 1,
+          order: -2,
+          todoListId: todoListID_2,
+        },
+        {
+          id: '2',
+          title: 'milk',
+          status: 2,
+          addedDate: new Date().toISOString(),
+          deadline: null,
+          description: null,
+          startDate: new Date().toISOString(),
+          priority: 1,
+          order: -1,
+          todoListId: todoListID_2,
+        },
+        {
+          id: '3',
+          title: 'tea',
+          status: 0,
+          addedDate: new Date().toISOString(),
+          deadline: null,
+          description: null,
+          startDate: new Date().toISOString(),
+          priority: 1,
+          order: 0,
+          todoListId: todoListID_2,
+        },
       ],
     }
   })
@@ -30,30 +96,34 @@ describe('tasksReducer', () => {
     const action = tasksActions.removeTask(todoListID_2, '2')
     const endState = tasksReducer(initialState, action)
 
-    expect(endState).toEqual({
-      [todoListID_1]: [
-        { id: '1', title: 'CSS', isDone: false },
-        { id: '2', title: 'JS', isDone: true },
-        { id: '3', title: 'React', isDone: false },
-      ],
-      [todoListID_2]: [
-        { id: '1', title: 'bread', isDone: false },
-        { id: '3', title: 'tea', isDone: false },
-      ],
-    })
+    expect(endState[todoListID_1]).toHaveLength(3)
+    expect(endState[todoListID_2]).toHaveLength(2)
+    expect(endState[todoListID_2][0].title).toBe('bread')
+    expect(endState[todoListID_2][1].title).toBe('tea')
   })
 
   it('correct task should be added to correct array', () => {
     const newTaskTitle = 'New Task'
 
-    const action = tasksActions.addTask(todoListID_2, newTaskTitle)
+    const action = tasksActions.addTask({
+      id: 'test-id',
+      title: newTaskTitle,
+      status: 0,
+      addedDate: new Date().toISOString(),
+      deadline: null,
+      description: null,
+      startDate: null,
+      priority: 1,
+      order: -3,
+      todoListId: todoListID_2,
+    })
     const endState = tasksReducer(initialState, action)
 
     expect(endState[todoListID_1]).toHaveLength(3)
     expect(endState[todoListID_2]).toHaveLength(4)
     expect(endState[todoListID_2][0].id).toBeDefined()
     expect(endState[todoListID_2][0].title).toBe(newTaskTitle)
-    expect(endState[todoListID_2][0].isDone).toBeFalsy()
+    expect(endState[todoListID_2][0].status).toBe(0)
   })
 
   it('title of specified task should be changed', () => {
@@ -67,11 +137,11 @@ describe('tasksReducer', () => {
   })
 
   it('status of specified task should be changed', () => {
-    const action = tasksActions.changeStatusTask(todoListID_2, '2', false)
+    const action = tasksActions.changeStatusTask(todoListID_2, '2', 0)
     const endState = tasksReducer(initialState, action)
 
-    expect(endState[todoListID_1][1].isDone).toBeTruthy()
-    expect(endState[todoListID_2][1].isDone).toBeFalsy()
+    expect(endState[todoListID_1][1].status).toBe(2)
+    expect(endState[todoListID_2][1].status).toBe(0)
   })
 
   it('property with todolistId should be deleted', () => {
