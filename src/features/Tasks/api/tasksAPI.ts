b7ef88ea9d42d3e92bbdcfeb72109251516e354d
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-07 20:32:12 +0300

    Created tasksAPI.ts

---
+++ src/features/Tasks/api/tasksAPI.ts
@@ -0,0 +1,40 @@
+import { AxiosResponse } from 'axios'
+
+import { instance, ResponseType } from '../../../app/api/api.ts'
+import { TaskType, UpdateTaskModelType } from '../model/types.ts'
+
+export const tasksAPI = {
+  getTasks(todoListID: string) {
+    return instance.get<GetTasksResponseType>(`todo-lists/${todoListID}/tasks`)
+  },
+  deleteTask(todoListID: string, taskID: string) {
+    return instance.delete<ResponseType>(`todo-lists/${todoListID}/tasks/${taskID}`)
+  },
+  createTask(todoListID: string, title: string) {
+    return instance.post<
+      null,
+      AxiosResponse<ResponseType<{ item: TaskType }>>,
+      {
+        title: string
+      }
+    >(`todo-lists/${todoListID}/tasks`, { title })
+  },
+  updateTask(todoListID: string, taskID: string, model: UpdateTaskModelType) {
+    return instance.put<
+      null,
+      AxiosResponse<
+        ResponseType<{
+          item: TaskType
+        }>
+      >,
+      UpdateTaskModelType
+    >(`todo-lists/${todoListID}/tasks/${taskID}`, model)
+  },
+}
+
+// TYPES
+type GetTasksResponseType = {
+  error: string | null
+  totalCount: number
+  items: TaskType[]
+}
