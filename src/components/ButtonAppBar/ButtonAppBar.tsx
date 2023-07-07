0a37fb03d000bf5713fb2ee7de10df3e1f69163e
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-08 02:41:57 +0300

    Added useAppSelector (status) and LinearProgress component

--- src/components/ButtonAppBar/ButtonAppBar.tsx
+++ src/components/ButtonAppBar/ButtonAppBar.tsx
@@ -3,12 +3,18 @@ import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
+import LinearProgress from '@mui/material/LinearProgress'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 
+import { useAppSelector } from '../../app/hooks/hooks.ts'
+import { RequestStatusType } from '../../app/model/types.ts'
+
 export const ButtonAppBar = () => {
+  const status = useAppSelector<RequestStatusType>(state => state.app.status)
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1, position: 'relative' }}>
       <AppBar position="static">
         <Toolbar>
           <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
@@ -20,6 +26,12 @@ export const ButtonAppBar = () => {
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+      {status === 'loading' && (
+        <LinearProgress
+          color="secondary"
+          sx={{ position: 'absolute', bottom: '-7%', left: 0, width: '100%' }}
+        />
+      )}
     </Box>
   )
 }
