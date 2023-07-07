496ae6700472f81008b475dbe254a562ff635718
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-08 02:41:32 +0300

    added 'setAppStatus' & 'setAppError' action creators and cases

--- src/app/model/actions.ts
+++ src/app/model/actions.ts
@@ -1,4 +1,19 @@
-export const appActions = {}
+import { RequestStatusType } from './types.ts'
+
+export const appActions = {
+  setAppStatus(status: RequestStatusType) {
+    return {
+      type: 'APP/SET-STATUS',
+      payload: { status },
+    } as const
+  },
+  setAppError(error: string | null) {
+    return {
+      type: 'APP/SET-ERROR',
+      payload: { error },
+    } as const
+  },
+}
 
 // TYPES
 export type AppActionsType = typeof appActions
--- src/app/model/appReducer.ts
+++ src/app/model/appReducer.ts
@@ -2,12 +2,16 @@ import { AppActionsType } from './actions.ts'
 import { RequestStatusType } from './types.ts'
 
 const initialState = {
-  error: null,
   status: 'idle' as RequestStatusType,
+  error: null as string | null,
 }
 
 export const appReducer = (state = initialState, action: ActionsType): InitialStateType => {
   switch (action.type) {
+    case 'APP/SET-STATUS':
+      return { ...state, status: action.payload.status }
+    case 'APP/SET-ERROR':
+      return { ...state, error: action.payload.error }
     default:
       return state
   }
