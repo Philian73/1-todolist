c0818b59ee81b05536a856720b7a4b7470a2e84c
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-08 04:09:51 +0300

    refactor createTodoList & deleteTodoList to async/await and import APIResultCodes & errorAPIHandler

--- src/features/TodoLists/model/thunks.ts
+++ src/features/TodoLists/model/thunks.ts
@@ -1,8 +1,9 @@
 import { Dispatch } from 'redux'
 
+import { APIResultCodes } from '../../../app/api/api.ts'
 import { appActions } from '../../../app/model/actions.ts'
 import { AppThunkType } from '../../../app/store/store.ts'
-import { handlerServerNetworkError } from '../../../app/utils/error-handler.ts'
+import { errorAPIHandler, handlerServerNetworkError } from '../../../app/utils/error-handler.ts'
 import { tasksThunks } from '../../Tasks/model/thunks.ts'
 import { todoListsAPI } from '../api/todoListsAPI.ts'
 
@@ -27,18 +28,39 @@ export const todoListsThunks = {
       }
     }
   },
-  deleteTodoList(ID: string) {
-    return (dispatch: Dispatch) => {
-      todoListsAPI.deleteTodoList(ID).then(() => {
+  deleteTodoList(ID: string): AppThunkType {
+    return async dispatch => {
+      dispatch(appActions.setAppStatus('loading'))
+      dispatch(todoListsActions.updateTodoList(ID, { entityStatus: 'loading' }))
+
+      try {
+        await todoListsAPI.deleteTodoList(ID)
+
         dispatch(todoListsActions.deleteTodoList(ID))
-      })
+        dispatch(appActions.setAppStatus('succeeded'))
+      } catch (error) {
+        handlerServerNetworkError(error, dispatch)
+      } finally {
+        dispatch(todoListsActions.updateTodoList(ID, { entityStatus: 'idle' }))
+      }
     }
   },
-  createTodoList(title: string) {
-    return (dispatch: Dispatch) => {
-      todoListsAPI.createTodoList(title).then(response => {
-        dispatch(todoListsActions.createTodoList(response.data.data.item))
-      })
+  createTodoList(title: string): AppThunkType {
+    return async dispatch => {
+      dispatch(appActions.setAppStatus('loading'))
+
+      try {
+        const response = await todoListsAPI.createTodoList(title)
+
+        if (response.data.resultCode === APIResultCodes.SUCCESS) {
+          dispatch(todoListsActions.createTodoList(response.data.data.item))
+          dispatch(appActions.setAppStatus('succeeded'))
+        } else {
+          errorAPIHandler(response.data, dispatch)
+        }
+      } catch (error) {
+        handlerServerNetworkError(error, dispatch)
+      }
     }
   },
   updateTitleTodoList(ID: string, title: string) {
