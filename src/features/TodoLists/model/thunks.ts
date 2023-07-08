1b4ba9ee9d7079d1b5f14b8d1e32c836e8c6090c
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-08 03:27:43 +0300

    getTodoLists thunk creator refactor to async/await and added dispatch setAppStatus and used handleServerNetworkError in catch

--- src/features/TodoLists/model/thunks.ts
+++ src/features/TodoLists/model/thunks.ts
@@ -1,6 +1,8 @@
 import { Dispatch } from 'redux'
 
+import { appActions } from '../../../app/model/actions.ts'
 import { AppThunkType } from '../../../app/store/store.ts'
+import { handlerServerNetworkError } from '../../../app/utils/error-handler.ts'
 import { tasksThunks } from '../../Tasks/model/thunks.ts'
 import { todoListsAPI } from '../api/todoListsAPI.ts'
 
@@ -8,14 +10,21 @@ import { todoListsActions } from './actions.ts'
 
 export const todoListsThunks = {
   getTodoLists(): AppThunkType {
-    return dispatch => {
-      todoListsAPI.getTodoLists().then(response => {
+    return async dispatch => {
+      dispatch(appActions.setAppStatus('loading'))
+
+      try {
+        const response = await todoListsAPI.getTodoLists()
+
+        dispatch(appActions.setAppStatus('succeeded'))
         dispatch(todoListsActions.setTodoLists(response.data))
 
         response.data.forEach(todoList => {
           dispatch(tasksThunks.getTasks(todoList.id))
         })
-      })
+      } catch (error) {
+        handlerServerNetworkError(error, dispatch)
+      }
     }
   },
   deleteTodoList(ID: string) {
