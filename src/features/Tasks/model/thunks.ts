8f13fb704393a3131b81f66778eee128309e8729
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-08 17:05:55 +0300

    async/await

--- src/features/Tasks/model/thunks.ts
+++ src/features/Tasks/model/thunks.ts
@@ -1,38 +1,62 @@
-import { Dispatch } from 'redux'
-
-import { AppRootStateType } from '../../../app/store/store.ts'
+import { APIResultCodes } from '../../../app/api/api.ts'
+import { appActions } from '../../../app/model/actions.ts'
+import { AppThunkType } from '../../../app/store/store.ts'
+import { errorAPIHandler, handlerServerNetworkError } from '../../../app/utils/error-handler.ts'
 import { tasksAPI } from '../api/tasksAPI.ts'
 
 import { tasksActions } from './actions.ts'
-import { UpdateTaskModelType } from './types.ts'
+import { TaskType, UpdateTaskModelType } from './types.ts'
 
 export const tasksThunks = {
-  getTasks(todoListID: string) {
-    return (dispatch: Dispatch) => {
-      tasksAPI.getTasks(todoListID).then(response => {
-        dispatch(tasksActions.setTasks(todoListID, response.data.items))
-      })
-    }
-  },
-  deleteTask(todoListID: string, taskID: string) {
-    return (dispatch: Dispatch) => {
-      tasksAPI.deleteTask(todoListID, taskID).then(() => {
-        dispatch(tasksActions.deleteTask(todoListID, taskID))
-      })
-    }
-  },
-  createTask(todoListID: string, title: string) {
-    return (dispatch: Dispatch) => {
-      tasksAPI.createTask(todoListID, title).then(response => {
-        dispatch(tasksActions.createTask(response.data.data.item))
-      })
-    }
-  },
-  updateTask(todoListID: string, taskID: string, data: Partial<UpdateTaskModelType>) {
-    return (dispatch: Dispatch, getState: () => AppRootStateType) => {
-      const task = getState().tasks[todoListID].find(task => task.id === taskID)
+  getTasks(todoListID: string): AppThunkType {
+    return async dispatch => {
+      const response = await tasksAPI.getTasks(todoListID)
 
-      if (task) {
+      dispatch(tasksActions.setTasks(todoListID, response.data.items))
+    }
+  },
+  deleteTask(todoListID: string, taskID: string): AppThunkType {
+    return async dispatch => {
+      dispatch(appActions.setAppStatus('loading'))
+      dispatch(tasksActions.updateTask(todoListID, taskID, { entityStatus: 'loading' }))
+
+      try {
+        await tasksAPI.deleteTask(todoListID, taskID)
+
+        dispatch(tasksActions.deleteTask(todoListID, taskID))
+        dispatch(appActions.setAppStatus('succeeded'))
+      } catch (error) {
+        handlerServerNetworkError(error, dispatch)
+      } finally {
+        dispatch(tasksActions.updateTask(todoListID, taskID, { entityStatus: 'idle' }))
+      }
+    }
+  },
+  createTask(todoListID: string, title: string): AppThunkType {
+    return async dispatch => {
+      dispatch(appActions.setAppStatus('loading'))
+      try {
+        const response = await tasksAPI.createTask(todoListID, title)
+
+        if (response.data.resultCode === APIResultCodes.SUCCESS) {
+          dispatch(tasksActions.createTask(response.data.data.item))
+          dispatch(appActions.setAppStatus('succeeded'))
+        } else {
+          errorAPIHandler<{ item: TaskType }>(response.data, dispatch)
+        }
+      } catch (error) {
+        handlerServerNetworkError(error, dispatch)
+      }
+    }
+  },
+  updateTask(todoListID: string, taskID: string, data: Partial<UpdateTaskModelType>): AppThunkType {
+    return async (dispatch, getState) => {
+      dispatch(appActions.setAppStatus('loading'))
+      dispatch(tasksActions.updateTask(todoListID, taskID, { entityStatus: 'loading' }))
+
+      try {
+        const task = getState().tasks[todoListID].find(task => task.id === taskID)!
+
         const model: UpdateTaskModelType = {
           title: task.title,
           deadline: task.deadline,
@@ -40,12 +64,18 @@ export const tasksThunks = {
           priority: task.priority,
           description: task.description,
           status: task.status,
+          entityStatus: task.entityStatus,
           ...data,
         }
 
-        tasksAPI.updateTask(todoListID, taskID, model).then(() => {
-          dispatch(tasksActions.updateTask(todoListID, taskID, model))
-        })
+        await tasksAPI.updateTask(todoListID, taskID, model)
+
+        dispatch(tasksActions.updateTask(todoListID, taskID, model))
+        dispatch(appActions.setAppStatus('succeeded'))
+      } catch (error) {
+        handlerServerNetworkError(error, dispatch)
+      } finally {
+        dispatch(tasksActions.updateTask(todoListID, taskID, { entityStatus: 'idle' }))
       }
     }
   },
