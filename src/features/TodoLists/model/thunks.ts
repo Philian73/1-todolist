8eac101b667f56d624b04369fa53d0561d9e4fdc
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-08 04:57:06 +0300

    refactor updateTitleTodoList thunk creator to async/await

--- src/features/TodoLists/model/thunks.ts
+++ src/features/TodoLists/model/thunks.ts
@@ -1,5 +1,3 @@
-import { Dispatch } from 'redux'
-
 import { APIResultCodes } from '../../../app/api/api.ts'
 import { appActions } from '../../../app/model/actions.ts'
 import { AppThunkType } from '../../../app/store/store.ts'
@@ -8,6 +6,7 @@ import { tasksThunks } from '../../Tasks/model/thunks.ts'
 import { todoListsAPI } from '../api/todoListsAPI.ts'
 
 import { todoListsActions } from './actions.ts'
+import { TodoListType } from './types.ts'
 
 export const todoListsThunks = {
   getTodoLists(): AppThunkType {
@@ -56,18 +55,32 @@ export const todoListsThunks = {
           dispatch(todoListsActions.createTodoList(response.data.data.item))
           dispatch(appActions.setAppStatus('succeeded'))
         } else {
-          errorAPIHandler(response.data, dispatch)
+          errorAPIHandler<{ item: TodoListType }>(response.data, dispatch)
         }
       } catch (error) {
         handlerServerNetworkError(error, dispatch)
       }
     }
   },
-  updateTitleTodoList(ID: string, title: string) {
-    return (dispatch: Dispatch) => {
-      todoListsAPI.updateTitleTodoList(ID, title).then(() => {
-        dispatch(todoListsActions.updateTodoList(ID, { title }))
-      })
+  updateTitleTodoList(ID: string, title: string): AppThunkType {
+    return async dispatch => {
+      dispatch(appActions.setAppStatus('loading'))
+      dispatch(todoListsActions.updateTodoList(ID, { entityStatus: 'loading' }))
+
+      try {
+        const response = await todoListsAPI.updateTitleTodoList(ID, title)
+
+        if (response.data.resultCode === APIResultCodes.SUCCESS) {
+          dispatch(todoListsActions.updateTodoList(ID, { title }))
+          dispatch(appActions.setAppStatus('succeeded'))
+        } else {
+          errorAPIHandler(response.data, dispatch)
+        }
+      } catch (error) {
+        handlerServerNetworkError(error, dispatch)
+      } finally {
+        dispatch(todoListsActions.updateTodoList(ID, { entityStatus: 'idle' }))
+      }
     }
   },
 }
