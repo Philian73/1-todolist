db23bd7e83dac4a9b49e668af05af4e013bac181
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-08 04:11:36 +0300

    added "entityStatus: 'idle'"

--- src/features/TodoLists/model/todoListsReducer.ts
+++ src/features/TodoLists/model/todoListsReducer.ts
@@ -9,11 +9,15 @@ export const todoListsReducer = (
 ): TodoListDomainType[] => {
   switch (action.type) {
     case 'TODOLISTS/SET-TODOLISTS':
-      return action.payload.todoLists.map(todoList => ({ ...todoList, filter: 'all' }))
+      return action.payload.todoLists.map(todoList => ({
+        ...todoList,
+        filter: 'all',
+        entityStatus: 'idle',
+      }))
     case 'TODOLISTS/DELETE-TODOLIST':
       return state.filter(todoList => todoList.id !== action.payload.ID)
     case 'TODOLISTS/CREATE-TODOLIST':
-      return [{ ...action.payload.todoList, filter: 'all' }, ...state]
+      return [{ ...action.payload.todoList, filter: 'all', entityStatus: 'idle' }, ...state]
     case 'TODOLISTS/UPDATE-TODOLIST':
       return state.map(todoList =>
         todoList.id === action.payload.ID ? { ...todoList, ...action.payload.data } : todoList
