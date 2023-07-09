9e4c1ca6701b6005010a99f067703971d7ecf4f4
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-10 00:16:40 +0300

    added unit test for appReducer: setAppError case

---
+++ src/__tests__/reducers/appReducer.test.ts
@@ -0,0 +1,24 @@
+import { describe, beforeEach, it, expect } from 'vitest'
+
+import { appActions } from '../../app/model/actions.ts'
+import { appReducer, InitialStateType } from '../../app/model/appReducer.ts'
+
+describe('appReducer', () => {
+  let initialState: InitialStateType
+
+  beforeEach(() => {
+    initialState = {
+      status: 'idle',
+      error: null,
+    }
+  })
+
+  it('correct error message should be set', () => {
+    const errorMessage = 'some-error'
+
+    const action = appActions.setAppError(errorMessage)
+    const endState = appReducer(initialState, action)
+
+    expect(endState.error).toBe(errorMessage)
+  })
+})
