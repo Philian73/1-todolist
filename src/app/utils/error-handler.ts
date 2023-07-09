b4480189324c6e4596948440bf41f566007a369e
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-09 23:51:12 +0300

    refactor

--- src/app/utils/error-handler.ts
+++ src/app/utils/error-handler.ts
@@ -5,15 +5,16 @@ import { appActions } from '../model/actions.ts'
 import { AppDispatchType } from '../store/store.ts'
 
 export const errorAPIHandler = <D>(data: ResponseType<D>, dispatch: AppDispatchType) => {
-  dispatch(appActions.setAppError(data.messages.length ? data.messages[0] : 'Some error occurred'))
   dispatch(appActions.setAppStatus('failed'))
+  dispatch(appActions.setAppError(data.messages.length ? data.messages[0] : 'Some error occurred'))
 }
 
 export const handlerServerNetworkError = (error: unknown, dispatch: AppDispatchType) => {
+  dispatch(appActions.setAppStatus('failed'))
+
   if (isAxiosError<AxiosError>(error)) {
     dispatch(appActions.setAppError(error.response ? error.response.data.message : error.message))
   } else {
     dispatch(appActions.setAppError((error as Error).message))
   }
-  dispatch(appActions.setAppStatus('failed'))
 }
