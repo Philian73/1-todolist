78e52866b39502749e97e1f87ba4372414224d09
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-09 23:41:21 +0300

    import and used ErrorSnackbar component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
 import './styles/App.css'
 
 import { ButtonAppBar } from '../components/ButtonAppBar/ButtonAppBar.tsx'
+import { ErrorSnackbar } from '../components/ErrorSnackbar/ErrorSnackbar.tsx'
 import { TodoLists } from '../features/TodoLists/TodoLists.tsx'
 
 const App = () => {
   return (
     <div className="App">
+      <ErrorSnackbar />
       <ButtonAppBar />
       <TodoLists />
     </div>
