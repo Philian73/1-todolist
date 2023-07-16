9861d766950cf91217139b5dbc2e52a94b155d0e
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-16 21:55:16 +0300

    created index.ts for components

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { ErrorSnackbar } from './ErrorSnackbar/ErrorSnackbar.tsx'
+export { AddItemForm } from './AddItemForm/AddItemForm.tsx'
+export { ButtonAppBar } from './ButtonAppBar/ButtonAppBar.tsx'
+export { EditableSpan } from './EditableSpan/EditableSpan.tsx'
