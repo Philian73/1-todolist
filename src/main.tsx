d2625808a2e0fdae01b148bf3aa59a4df9eb0730
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-18 23:53:06 +0300

    Wrapped HashRouter (GH pages)

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
+import './app/styles/index.css'
+
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-
-import './app/styles/index.css'
+import { HashRouter } from 'react-router-dom'
 
 import App from './app/App.tsx'
 import { store } from './app/store/store.ts'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </Provider>
 )
