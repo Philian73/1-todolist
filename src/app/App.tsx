b47c1f2227bde5d37a063684003d025ac8419c4b
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-18 23:53:34 +0300

    Added routes: /login, / , 404, *

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,10 @@
 import './styles/App.css'
 
-import { ButtonAppBar } from '../components/ButtonAppBar/ButtonAppBar.tsx'
-import { ErrorSnackbar } from '../components/ErrorSnackbar/ErrorSnackbar.tsx'
+import Container from '@mui/material/Container'
+import { Navigate, Route, Routes } from 'react-router-dom'
+
+import { ButtonAppBar, ErrorSnackbar } from '../components'
+import { Login } from '../features/Auth'
 import { TodoLists } from '../features/TodoLists'
 
 const App = () => {
@@ -9,7 +12,16 @@ const App = () => {
     <div className="App">
       <ErrorSnackbar />
       <ButtonAppBar />
-      <TodoLists />
+      <Container fixed>
+        <Routes>
+          <Route path="/" element={<TodoLists />} />
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/404" element={<h1>404: PAGE NOT FOUND</h1>} />
+
+          <Route path="*" element={<Navigate to="/404" />} />
+        </Routes>
+      </Container>
     </div>
   )
 }
