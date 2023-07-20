dcfc31a85573afc239a94d0a0a7a1410f02dfe83
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-21 00:26:54 +0300

    added 'login' thunk creator

---
+++ src/features/Auth/model/thunks.ts
@@ -0,0 +1,29 @@
+import { APIResultCodes } from '../../../app/api/api.ts'
+import { appActions } from '../../../app/model/actions.ts'
+import { AppThunkType } from '../../../app/store/store.ts'
+import { errorAPIHandler, handlerServerNetworkError } from '../../../app/utils/error-handler.ts'
+import { authAPI } from '../api/authAPI.ts'
+
+import { authActions } from './actions.ts'
+import { LoginParamsType } from './types.ts'
+
+export const authThunks = {
+  login(data: LoginParamsType): AppThunkType {
+    return async dispatch => {
+      dispatch(appActions.setAppStatus('loading'))
+
+      try {
+        const response = await authAPI.login(data)
+
+        if (response.data.resultCode === APIResultCodes.SUCCESS) {
+          dispatch(authActions.setIsLoggedIn(true))
+          dispatch(appActions.setAppStatus('succeeded'))
+        } else {
+          errorAPIHandler(response.data, dispatch)
+        }
+      } catch (error) {
+        handlerServerNetworkError(error, dispatch)
+      }
+    }
+  },
+}
