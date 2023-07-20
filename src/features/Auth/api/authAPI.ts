a3151ce936f86dc10efe7ff5e3095b3df2a898c0
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-21 00:26:37 +0300

    added login method

--- src/features/Auth/api/authAPI.ts
+++ src/features/Auth/api/authAPI.ts
@@ -1,1 +1,13 @@
-export const authAPI = {}
+import { AxiosResponse } from 'axios'
+
+import { instance, ResponseType } from '../../../app/api/api.ts'
+import { LoginParamsType } from '../model/types.ts'
+
+export const authAPI = {
+  login(data: LoginParamsType) {
+    return instance.post<null, AxiosResponse<ResponseType<{ userId: number }>>, LoginParamsType>(
+      'auth/login',
+      data
+    )
+  },
+}
