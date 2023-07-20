731509ce71ff320adc28985de7d944e68ec61524
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-20 23:56:30 +0300

    added setIsLoggedIn action creator

--- src/features/Auth/model/actions.ts
+++ src/features/Auth/model/actions.ts
@@ -1,3 +1,10 @@
-export const authActions = {}
+export const authActions = {
+  setIsLoggedIn(value: boolean) {
+    return {
+      type: 'LOGIN/SET-IS-LOGGED-IN',
+      payload: { value },
+    } as const
+  },
+}
 
 export type AuthActionsType = typeof authActions
