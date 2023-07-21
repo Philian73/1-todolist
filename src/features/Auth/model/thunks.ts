4b86ef6633e15472d0f6282649651871eed4d5e9
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-21 03:57:15 +0300

    added 'auth/me' method

--- src/features/Auth/model/thunks.ts
+++ src/features/Auth/model/thunks.ts
@@ -8,6 +8,26 @@ import { authActions } from './actions.ts'
 import { LoginParamsType } from './types.ts'
 
 export const authThunks = {
+  me(): AppThunkType {
+    return async dispatch => {
+      dispatch(appActions.setAppStatus('loading'))
+
+      try {
+        const response = await authAPI.me()
+
+        if (response.data.resultCode === APIResultCodes.SUCCESS) {
+          dispatch(authActions.setIsLoggedIn(true))
+          dispatch(appActions.setAppStatus('succeeded'))
+        } else {
+          errorAPIHandler(response.data, dispatch)
+        }
+      } catch (error) {
+        handlerServerNetworkError(error, dispatch)
+      } finally {
+        dispatch(appActions.setAppIsInitialized(true))
+      }
+    }
+  },
   login(data: LoginParamsType): AppThunkType {
     return async dispatch => {
       dispatch(appActions.setAppStatus('loading'))
