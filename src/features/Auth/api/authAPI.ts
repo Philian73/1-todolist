7a12fed88280e9be3cd30406ca87c5572b4c74f4
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-21 03:57:09 +0300

    added 'auth/me' method

--- src/features/Auth/api/authAPI.ts
+++ src/features/Auth/api/authAPI.ts
@@ -1,9 +1,12 @@
 import { AxiosResponse } from 'axios'
 
 import { instance, ResponseType } from '../../../app/api/api.ts'
-import { LoginParamsType } from '../model/types.ts'
+import { LoginParamsType, UserDate } from '../model/types.ts'
 
 export const authAPI = {
+  me() {
+    return instance.get<ResponseType<UserDate>>('auth/me')
+  },
   login(data: LoginParamsType) {
     return instance.post<null, AxiosResponse<ResponseType<{ userId: number }>>, LoginParamsType>(
       'auth/login',
