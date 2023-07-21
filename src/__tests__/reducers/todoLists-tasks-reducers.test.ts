ef356166cb25b3c7beb5839d7bbce949ef09e295
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-21 17:35:35 +0300

    added unit-test for 'clearTodoLists' case

--- src/__tests__/reducers/todoLists-tasks-reducers.test.ts
+++ src/__tests__/reducers/todoLists-tasks-reducers.test.ts
@@ -1,7 +1,7 @@
-import { test, expect } from 'vitest'
+import { expect, test } from 'vitest'
 
 import { tasksReducer } from '../../features/Tasks/model/tasksReducer.ts'
-import { TasksType } from '../../features/Tasks/model/types.ts'
+import { TaskPriorities, TaskStatuses, TasksType } from '../../features/Tasks/model/types.ts'
 import { todoListsActions } from '../../features/TodoLists/model/actions.ts'
 import { todoListsReducer } from '../../features/TodoLists/model/todoListsReducer.ts'
 import { TodoListDomainType } from '../../features/TodoLists/model/types.ts'
@@ -26,4 +26,44 @@ test('ids should be equals', () => {
 
   expect(idFromTasks).toBe(action.payload.todoList.id)
   expect(idFromTodoLists).toBe(action.payload.todoList.id)
+})
+
+test('TodoLists and their tasks should been cleared', () => {
+  const initialTasksState: TasksType = {
+    ['1']: [
+      {
+        id: '1',
+        entityStatus: 'idle',
+        order: 0,
+        addedDate: new Date().toISOString(),
+        title: 'some-title',
+        startDate: '',
+        status: TaskStatuses.New,
+        deadline: '',
+        priority: TaskPriorities.Low,
+        description: '',
+        todoListId: '1',
+      },
+    ],
+  }
+  const initialTodoListsState: TodoListDomainType[] = [
+    {
+      id: '1',
+      title: 'test',
+      entityStatus: 'idle',
+      order: 0,
+      addedDate: new Date().toISOString(),
+      filter: 'all',
+    },
+  ]
+
+  const action = todoListsActions.clearTodoLists()
+
+  const endTasksState = tasksReducer(initialTasksState, action)
+  const endTodoListsState = todoListsReducer(initialTodoListsState, action)
+
+  const keys = Object.keys(endTasksState)
+
+  expect(keys).toHaveLength(0)
+  expect(endTodoListsState).toHaveLength(0)
 })
