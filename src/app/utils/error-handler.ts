e88351b85312754d812cce276654f995e6b68499
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-28 17:20:25 +0300

    temp refactor

--- src/app/utils/error-handler.ts
+++ src/app/utils/error-handler.ts
@@ -1,15 +1,15 @@
 import { AxiosError, isAxiosError } from 'axios'
+import { Dispatch } from 'redux'
 
-import { ResponseType } from '../api/api.ts'
-import { appActions } from '../model/actions.ts'
-import { AppDispatchType } from '../store/store.ts'
+import { ResponseType } from 'app/api/api.ts'
+import { appActions } from 'app/model/actions.ts'
 
-export const errorAPIHandler = <D>(data: ResponseType<D>, dispatch: AppDispatchType) => {
+export const errorAPIHandler = <D>(data: ResponseType<D>, dispatch: Dispatch) => {
   dispatch(appActions.setAppError(data.messages.length ? data.messages[0] : 'Some error occurred'))
   dispatch(appActions.setAppStatus('failed'))
 }
 
-export const handlerServerNetworkError = (error: unknown, dispatch: AppDispatchType) => {
+export const handlerServerNetworkError = (error: unknown, dispatch: Dispatch) => {
   if (isAxiosError<AxiosError>(error)) {
     dispatch(appActions.setAppError(error.response ? error.response.data.message : error.message))
   } else {
