4d747b2a5a09438aaebc34ad7bbe3c1bec40eb65
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-28 23:15:35 +0200

    created util func handlerServerNetworkError.ts

--- src/common/utils/error-handling/handlerServerNetworkError.ts
+++ src/common/utils/error-handling/handlerServerNetworkError.ts
@@ -1,13 +1,7 @@
 import { AxiosError, isAxiosError } from 'axios'
 import { Dispatch } from 'redux'
 
-import { ResponseType } from 'app/api/api.ts'
 import { appActions } from 'app/model/actions.ts'
-
-export const errorAPIHandler = <D>(data: ResponseType<D>, dispatch: Dispatch) => {
-  dispatch(appActions.setAppError(data.messages.length ? data.messages[0] : 'Some error occurred'))
-  dispatch(appActions.setAppStatus('failed'))
-}
 
 export const handlerServerNetworkError = (error: unknown, dispatch: Dispatch) => {
   if (isAxiosError<AxiosError>(error)) {
