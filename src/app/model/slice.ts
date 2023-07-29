9e21ffb2e2f987800e8243e81626c7fe8a552b25
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-29 23:04:02 +0200

    Created slice for App

---
+++ src/app/model/slice.ts
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+import { RequestStatusType } from './types.ts'
+
+export type AppInitialStateType = {
+  isInitialized: boolean
+  status: RequestStatusType
+  error: string | null
+}
+
+const initialState: AppInitialStateType = {
+  isInitialized: false,
+  status: 'idle',
+  error: null,
+}
+
+const slice = createSlice({
+  name: '@@app',
+  initialState,
+  reducers: {
+    setAppIsInitialized(state, action: PayloadAction<{ isInitialized: boolean }>) {
+      state.isInitialized = action.payload.isInitialized
+    },
+    setAppStatus(state, action: PayloadAction<{ status: RequestStatusType }>) {
+      state.status = action.payload.status
+    },
+    setAppError(state, action: PayloadAction<{ error: string | null }>) {
+      state.error = action.payload.error
+    },
+  },
+})
+
+export const appReducer = slice.reducer
+export const appActions = slice.actions
