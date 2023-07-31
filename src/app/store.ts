723a6d618276c95968a2a51d800f2be40eeb0b62
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-07-31 17:47:34 +0300

    refactor: deleted rootReducer (combine)

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,20 +1,18 @@
 import { configureStore, ThunkAction } from '@reduxjs/toolkit'
-import { AnyAction, combineReducers } from 'redux'
+import { AnyAction } from 'redux'
 
 import { appReducer } from 'app/model/slice.ts'
 import { authReducer } from 'features/Auth/model/slice.ts'
 import { tasksReducer } from 'features/Tasks/model/slice.ts'
 import { todoListsReducer } from 'features/TodoLists/model/slice.ts'
 
-const rootReducer = combineReducers({
-  app: appReducer,
-  auth: authReducer,
-  tasks: tasksReducer,
-  todoLists: todoListsReducer,
-})
-
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    app: appReducer,
+    auth: authReducer,
+    todoLists: todoListsReducer,
+    tasks: tasksReducer,
+  },
 })
 
 // TYPES
