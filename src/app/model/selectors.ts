07940fc0fd267a440985b56907044b94737ccd8f
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-08-01 23:07:48 +0000

    Added 'selectStatus' for App selectors.ts

--- src/app/model/selectors.ts
+++ src/app/model/selectors.ts
@@ -2,4 +2,5 @@ import { AppRootStateType } from 'app/store.ts'
 
 export const appSelectors = {
   selectIsInitialized: (state: AppRootStateType) => state.app.isInitialized,
+  selectStatus: (state: AppRootStateType) => state.app.status,
 }
