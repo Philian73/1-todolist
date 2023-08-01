4be2b331cdaa57c8cf610eddb408375af08d774b
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-08-01 23:48:47 +0000

    added 'selectTasksByTodoListID' in selectors.ts for Tasks

--- src/features/Tasks/model/selectors.ts
+++ src/features/Tasks/model/selectors.ts
@@ -1,1 +1,6 @@
-export const tasksSelectors = {}
+import { AppRootStateType } from 'app/store.ts'
+
+export const tasksSelectors = {
+  selectTasksByTodoListID: (todoListID: string) => (state: AppRootStateType) =>
+    state.tasks[todoListID],
+}
