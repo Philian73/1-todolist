4c1e88d27e3f488fd39d4d537f96adae22a5b0ce
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-08-01 23:07:56 +0000

    Added 'selectError' for App selectors.ts

--- src/app/model/selectors.ts
+++ src/app/model/selectors.ts
@@ -3,4 +3,5 @@ import { AppRootStateType } from 'app/store.ts'
 export const appSelectors = {
   selectIsInitialized: (state: AppRootStateType) => state.app.isInitialized,
   selectStatus: (state: AppRootStateType) => state.app.status,
+  selectError: (state: AppRootStateType) => state.app.error,
 }
