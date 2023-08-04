800a8480afe337f1d28e484eab64b95f419c1269
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-08-04 17:41:55 +0300

    refactor

--- src/common/utils/error-handling/handlerServerNetworkError.ts
+++ src/common/utils/error-handling/handlerServerNetworkError.ts
@@ -4,13 +4,16 @@ import { Dispatch } from 'redux'
 import { appActions } from 'app/model/slice.ts'
 
 export const handlerServerNetworkError = (error: unknown, dispatch: Dispatch) => {
+  let errorMessage = 'Some error occurred'
+
   if (isAxiosError(error)) {
-    dispatch(appActions.setAppError({ error: error.response?.data.message || error.message }))
+    errorMessage = error.response?.data?.message || error?.message || errorMessage
   } else if (error instanceof Error) {
-    dispatch(appActions.setAppError({ error: error.message }))
+    errorMessage = `Native error: ${error.message}`
   } else {
-    dispatch(appActions.setAppError({ error: 'Some occurred error' }))
+    errorMessage = JSON.stringify(error)
   }
 
+  dispatch(appActions.setAppError({ error: errorMessage }))
   dispatch(appActions.setAppStatus({ status: 'failed' }))
 }
