158f07f68260324070777dea3f8959d2022eb476
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-08-06 17:15:39 +0300

    refactor

--- src/features/Auth/model/slice.ts
+++ src/features/Auth/model/slice.ts
@@ -8,6 +8,68 @@ import { authAPI } from 'features/Auth/api'
 import { LoginParamsType } from 'features/Auth/model/types.ts'
 import { todoListsActions } from 'features/TodoLists/model/slice.ts'
 
+// THUNKS
+const me = (): AppThunkType => {
+  return async dispatch => {
+    dispatch(appActions.setAppStatus({ status: 'loading' }))
+
+    try {
+      const response = await authAPI.me()
+
+      if (response.data.resultCode === APIResultCodes.SUCCESS) {
+        dispatch(authActions.setIsLoggedIn({ isLoggedIn: true }))
+        dispatch(appActions.setAppStatus({ status: 'succeeded' }))
+      } else {
+        errorAPIHandler(response.data, dispatch)
+      }
+    } catch (error) {
+      handlerServerNetworkError(error, dispatch)
+    } finally {
+      dispatch(appActions.setAppIsInitialized({ isInitialized: true }))
+    }
+  }
+}
+
+const login = (data: LoginParamsType): AppThunkType => {
+  return async dispatch => {
+    dispatch(appActions.setAppStatus({ status: 'loading' }))
+
+    try {
+      const response = await authAPI.login(data)
+
+      if (response.data.resultCode === APIResultCodes.SUCCESS) {
+        dispatch(authActions.setIsLoggedIn({ isLoggedIn: true }))
+        dispatch(appActions.setAppStatus({ status: 'succeeded' }))
+      } else {
+        errorAPIHandler(response.data, dispatch)
+      }
+    } catch (error) {
+      handlerServerNetworkError(error, dispatch)
+    }
+  }
+}
+
+const logout = (): AppThunkType => {
+  return async dispatch => {
+    dispatch(appActions.setAppStatus({ status: 'loading' }))
+
+    try {
+      const response = await authAPI.logout()
+
+      if (response.data.resultCode === APIResultCodes.SUCCESS) {
+        dispatch(authActions.setIsLoggedIn({ isLoggedIn: false }))
+        dispatch(appActions.setAppStatus({ status: 'succeeded' }))
+        dispatch(todoListsActions.clearTodoLists())
+      } else {
+        errorAPIHandler(response.data, dispatch)
+      }
+    } catch (error) {
+      handlerServerNetworkError(error, dispatch)
+    }
+  }
+}
+
+// SLICE
 const initialState: AuthInitialStateType = {
   isLoggedIn: false,
 }
@@ -24,65 +86,7 @@ const slice = createSlice({
 
 export const authReducer = slice.reducer
 export const authActions = slice.actions
-export const authThunks = {
-  me(): AppThunkType {
-    return async dispatch => {
-      dispatch(appActions.setAppStatus({ status: 'loading' }))
-
-      try {
-        const response = await authAPI.me()
-
-        if (response.data.resultCode === APIResultCodes.SUCCESS) {
-          dispatch(authActions.setIsLoggedIn({ isLoggedIn: true }))
-          dispatch(appActions.setAppStatus({ status: 'succeeded' }))
-        } else {
-          errorAPIHandler(response.data, dispatch)
-        }
-      } catch (error) {
-        handlerServerNetworkError(error, dispatch)
-      } finally {
-        dispatch(appActions.setAppIsInitialized({ isInitialized: true }))
-      }
-    }
-  },
-  login(data: LoginParamsType): AppThunkType {
-    return async dispatch => {
-      dispatch(appActions.setAppStatus({ status: 'loading' }))
-
-      try {
-        const response = await authAPI.login(data)
-
-        if (response.data.resultCode === APIResultCodes.SUCCESS) {
-          dispatch(authActions.setIsLoggedIn({ isLoggedIn: true }))
-          dispatch(appActions.setAppStatus({ status: 'succeeded' }))
-        } else {
-          errorAPIHandler(response.data, dispatch)
-        }
-      } catch (error) {
-        handlerServerNetworkError(error, dispatch)
-      }
-    }
-  },
-  logout(): AppThunkType {
-    return async dispatch => {
-      dispatch(appActions.setAppStatus({ status: 'loading' }))
-
-      try {
-        const response = await authAPI.logout()
-
-        if (response.data.resultCode === APIResultCodes.SUCCESS) {
-          dispatch(authActions.setIsLoggedIn({ isLoggedIn: false }))
-          dispatch(appActions.setAppStatus({ status: 'succeeded' }))
-          dispatch(todoListsActions.clearTodoLists())
-        } else {
-          errorAPIHandler(response.data, dispatch)
-        }
-      } catch (error) {
-        handlerServerNetworkError(error, dispatch)
-      }
-    }
-  },
-}
+export const authThunks = { me, login, logout }
 
 // TYPES
 export type AuthInitialStateType = {
