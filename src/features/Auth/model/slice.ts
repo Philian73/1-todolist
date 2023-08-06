db411bb5728f2f98aaa02c6f94a5600e8f2d131b
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-08-06 17:20:23 +0300

    refactor: 'me' thunk creator (createAppAsyncThunk)

--- src/features/Auth/model/slice.ts
+++ src/features/Auth/model/slice.ts
@@ -1,34 +1,40 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 
 import { appActions } from 'app/model/slice.ts'
 import { AppThunkType } from 'app/store.ts'
 import { APIResultCodes } from 'common/api'
-import { errorAPIHandler, handlerServerNetworkError } from 'common/utils'
+import { createAppAsyncThunk, errorAPIHandler, handlerServerNetworkError } from 'common/utils'
 import { authAPI } from 'features/Auth/api'
 import { LoginParamsType } from 'features/Auth/model/types.ts'
 import { todoListsActions } from 'features/TodoLists/model/slice.ts'
 
 // THUNKS
-const me = (): AppThunkType => {
-  return async dispatch => {
+const me = createAppAsyncThunk<{ isLoggedIn: boolean }, any>(
+  '@@auth/me',
+  async (_, { dispatch, rejectWithValue }) => {
     dispatch(appActions.setAppStatus({ status: 'loading' }))
 
     try {
       const response = await authAPI.me()
 
       if (response.data.resultCode === APIResultCodes.SUCCESS) {
-        dispatch(authActions.setIsLoggedIn({ isLoggedIn: true }))
         dispatch(appActions.setAppStatus({ status: 'succeeded' }))
+
+        return { isLoggedIn: true }
       } else {
         errorAPIHandler(response.data, dispatch)
+
+        return rejectWithValue(null)
       }
     } catch (error) {
       handlerServerNetworkError(error, dispatch)
+
+      return rejectWithValue(null)
     } finally {
       dispatch(appActions.setAppIsInitialized({ isInitialized: true }))
     }
   }
-}
+)
 
 const login = (data: LoginParamsType): AppThunkType => {
   return async dispatch => {
@@ -77,10 +83,11 @@ const initialState: AuthInitialStateType = {
 const slice = createSlice({
   name: '@@auth',
   initialState,
-  reducers: {
-    setIsLoggedIn: (state, action: PayloadAction<{ isLoggedIn: boolean }>) => {
+  reducers: {},
+  extraReducers: builder => {
+    builder.addCase(me.fulfilled, (state, action) => {
       state.isLoggedIn = action.payload.isLoggedIn
-    },
+    })
   },
 })
 
