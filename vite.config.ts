8b6b3550dbb99118bfb70af040128fab632d87a5
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-08-07 22:45:43 -0500

    import 'path' and added alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
+import * as path from 'path'
+
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
   build: {
     outDir: 'build',
   },
