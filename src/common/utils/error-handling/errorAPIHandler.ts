700a67b8aa585cc9f73a78ef564a6ef4d3e6a345
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-08-09 23:29:43 -0700

    added 'show' error param + refactor

--- src/common/utils/error-handling/errorAPIHandler.ts
+++ src/common/utils/error-handling/errorAPIHandler.ts
@@ -1,13 +1,19 @@
-import { Dispatch } from 'redux'
-
 import { appActions } from '@/app/model'
+import { AppDispatchType } from '@/app/store'
 import { ResponseType } from '@/common/api'
 
-export const errorAPIHandler = <D>(data: ResponseType<D>, dispatch: Dispatch) => {
-  dispatch(
-    appActions.setAppError({
-      error: data.messages.length ? data.messages[0] : 'Some error occurred',
-    })
-  )
+export const errorAPIHandler = <D>(
+  data: ResponseType<D>,
+  dispatch: AppDispatchType,
+  showError: boolean = true
+) => {
+  if (showError) {
+    dispatch(
+      appActions.setAppError({
+        error: data.messages.length ? data.messages[0] : 'Some error occurred',
+      })
+    )
+  }
+
   dispatch(appActions.setAppStatus({ status: 'failed' }))
 }
