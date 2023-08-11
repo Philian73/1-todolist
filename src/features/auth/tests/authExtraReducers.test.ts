8442b9ed18a2ed7a7c3f7deb154314482c04e14f
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-08-11 09:29:58 +0300

    added test for auth slice extra-reducers

---
+++ src/features/auth/tests/authExtraReducers.test.ts
@@ -0,0 +1,36 @@
+import { describe, beforeEach, it, expect } from 'vitest'
+
+import { AuthInitialStateType, authReducer, authThunks } from '../model'
+
+import { createFulfilledAction } from '@/common/utils'
+
+describe('authExtraReducers', () => {
+  let initialState: AuthInitialStateType
+
+  beforeEach(() => {
+    initialState = {
+      isLoggedIn: false,
+    }
+  })
+
+  it('isLoggedIn set to true on authThunks.me()', () => {
+    const action = createFulfilledAction(authThunks.me)
+    const endState = authReducer(initialState, action)
+
+    expect(endState.isLoggedIn).toBeTruthy()
+  })
+
+  it('isLoggedIn set to true on authThunks.login()', () => {
+    const action = createFulfilledAction(authThunks.login)
+    const endState = authReducer(initialState, action)
+
+    expect(endState.isLoggedIn).toBeTruthy()
+  })
+
+  it('isLoggedIn set to false on authThunks.logout()', () => {
+    const action = createFulfilledAction(authThunks.logout)
+    const endState = authReducer(initialState, action)
+
+    expect(endState.isLoggedIn).toBeFalsy()
+  })
+})
