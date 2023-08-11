c5faf7a1e4a87bbdf648aa81702516329d48fd09
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-08-10 23:42:12 -0500

    added util func 'createFulfilledAction'

---
+++ src/common/utils/tests/createFulfilledAction.ts
@@ -0,0 +1,9 @@
+export const createFulfilledAction = <T extends { fulfilled: any }>(
+  action: T,
+  payload: ReturnType<T['fulfilled']>['payload']
+) => {
+  return {
+    type: action.fulfilled.type,
+    payload,
+  }
+}
